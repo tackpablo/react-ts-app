a4314e92aa8877e755460df432d9f5d9770bee1e
Pablo <tackpablo@gmail.com>
2022-05-06 09:47:59 -0700

    Added data models using class and constructor

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
+import Todo from "../models/todo";
+
 type TodosProps = {
-  items: string[];
+  items: Todo[];
   children?: ReactNode;
 };
 
@@ -8,7 +10,7 @@ const Todos = (props: TodosProps) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
