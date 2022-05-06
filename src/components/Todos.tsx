e30a8e0c198392b2a71c74c50e397a532a436ef6
Pablo <tackpablo@gmail.com>
2022-05-06 09:31:12 -0700

    Add props using ts

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,17 @@
-function Todos() {
+import { ReactNode } from "react";
+type TodosProps = {
+  items: string[];
+  children?: ReactNode;
+};
+
+const Todos = (props: TodosProps) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn Typescript</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
