eb6071f07b2d30e732009d91621c85b0c0fa0e25
Pablo <tackpablo@gmail.com>
2022-05-07 16:33:22 -0700

    Add new todo submission form

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,11 @@
+const NewTodo = () => {
+  return (
+    <form>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
