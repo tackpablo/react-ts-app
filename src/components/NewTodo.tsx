7b5c9e2c012e5ddcf8f449dcc126e73e45dc84b3
Pablo <tackpablo@gmail.com>
2022-05-07 16:37:08 -0700

    Add type to event

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,12 @@
+import { FormEvent } from "react";
+
 const NewTodo = () => {
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
       <input type="text" id="text" />
       <button>Add Todo</button>
