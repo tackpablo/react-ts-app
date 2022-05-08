bb9220092336657958c21da8157f639ba40cf0b3
Pablo <tackpablo@gmail.com>
2022-05-07 19:09:21 -0700

    Add removing of todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,11 +3,16 @@ import classes from "./TodoItem.module.css";
 
 type TodoItemProps = {
   text: string;
+  onRemoveTodo: () => void;
   children?: ReactNode;
 };
 
 const TodoItem = (props: TodoItemProps) => {
-  return <li className={classes.item}>{props.text}</li>;
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -5,6 +5,7 @@ import classes from "./Todos.module.css";
 
 type TodosProps = {
   items: Todo[];
+  onRemoveTodo: (id: string) => void;
   children?: ReactNode;
 };
 
@@ -12,7 +13,11 @@ const Todos = (props: TodosProps) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={() => props.onRemoveTodo(item.id)}
+        />
       ))}
     </ul>
   );
